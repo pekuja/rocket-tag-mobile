6a0af81ceda4169f14640777e1b610c23493adfb
pekuja <pekuja@iki.fi>
2025-02-12 19:22:04 +0200

    Added an aiming guide line that shows where the projectile will fly
    This actually immediately makes aiming feel better. I might want to add something similar for the grappling hook as well, though I'm not sure how to differentiate the two clearly.

--- scenes/local_player.gd
+++ scenes/local_player.gd
@@ -6,13 +6,15 @@ class_name LocalPlayer
 @export var shootInput : TouchJoystick
 
 @export var projectileSpeed = 400.0
+const PROJECTILE_LIFETIME = 1.0
 
 @export var grapplingHookSpeed = 400.0
 
 @onready var character = $Character
 @onready var _projectile_spawn_point = $Character/GunSprite/ProjectileSpawnPoint
 @onready var _sprite_gun = $Character/GunSprite
-@onready var _arrow = $Character/Line2D
+@onready var _arrow = $Character/Arrow
+@onready var _aim_guide = $AimGuide
 
 var gravity: int = ProjectSettings.get("physics/2d/default_gravity")
 
@@ -47,3 +49,19 @@ func _process(_delta: float) -> void:
 		_arrow.rotation = target_rotation
 	if shootInput.is_pressed and shootInput.joystick_position.length() > SHOOT_THRESHOLD:
 		_sprite_gun.global_rotation = Vector2(0, -1).angle_to(shootInput.joystick_position)
+		
+		_aim_guide.visible = true
+		_aim_guide.points[0] = _projectile_spawn_point.global_position - global_position
+		var projectile_range = projectileSpeed * PROJECTILE_LIFETIME
+		var end_pos = _projectile_spawn_point.global_position + shootInput.joystick_position.normalized() * projectile_range
+		var space_state = get_world_2d().direct_space_state
+		var query = PhysicsRayQueryParameters2D.create(_projectile_spawn_point.global_position, end_pos)
+		query.exclude = [character]
+		var result = space_state.intersect_ray(query)
+		
+		if result:
+			_aim_guide.points[1] = result.position - global_position
+		else:
+			_aim_guide.points[1] = end_pos - global_position
+	else:
+		_aim_guide.visible = false
